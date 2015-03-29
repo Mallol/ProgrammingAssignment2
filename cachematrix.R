51ddf7dd5d652c2391252388df3a78036b43d68c
Luiz Mallol <luiz.mallol@gmail.com>
2015-03-29 01:52:28 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Funcao para criacao de matriz com cache
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(matriz_origem = matrix()) {
+        
+        inv_matriz <- NULL
+        
+        set <- function(dados) {
+                matriz_origem <<- dados
+                inv_matriz <<- NULL
+        }
+        
+## Funcao para setar e obter matriz invertida
+        get <- function() matriz_origem
 
-makeCacheMatrix <- function(x = matrix()) {
-
+        set_inverse <- function(solve) inv_matriz <<- solve
+        get_inverse <- function() inv_matriz
+        
+        list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## testa se o inverso da matriz foi armazenada, se sim imprime matriz caheada
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matriz_no_cache, ...) {
+        inv_matriz <- matriz_no_cache$get_inverse()
+        
+## testa se a matriz esta no cache
+        if(!is.null(inv_matriz)) { 
+                print("Matriz Cacheada")
+                return(inv_matriz)
+        }
+        
+## Matriz nao esta no cache
+        matriz <- matriz_no_cache$get()
+        inv_matriz <- solve(matriz)
+        matriz_no_cache$set_inverse(inv_matriz)
+        inv_matriz
+        print("Matriz nao estava no cache")
 }
+
+
+
+## t e s t e    d e      pr o g r a m a ##
+
+## cria matriz para verificacao
+matriz_de_teste <- matrix(runif(100,1,10),10,10)
+##chama a funcao para fazer o cache
+esta_no_cache <- makeCacheMatrix(matriz_de_teste)
+## primera vez fora do cache, nas proximas no cache
+retorno <- cacheSolve(esta_no_cache)
+retorno <- cacheSolve(esta_no_cache)
+
+
